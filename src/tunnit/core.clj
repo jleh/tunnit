199b3ef0ac6100cdfeeeb659229d50205f44f247
Juuso Lehtinen <jupelehtinen@gmail.com>
2018-02-11 20:01:12 +0200

    rename all using lisp-case

--- src/tunnit/core.clj
+++ src/tunnit/core.clj
@@ -12,65 +12,65 @@
 (def cli-options [["-f" "--file FILE" "Filename"]
                   ["-d" "--diff DIFF" "Initial diff in minutes" :default 0]])
 
-(defn getTime [timeStr]
-  (f/parse custom-formatter (str "2018-02-01 " timeStr)))
+(defn get-time [time-str]
+  (f/parse custom-formatter (str "2018-02-01 " time-str)))
 
-(defn getMinutes [times]
+(defn get-minutes [times]
   (t/in-minutes
-    (t/interval (getTime (get times 0)) (getTime (get times 1)))))
+    (t/interval (get-time (get times 0)) (get-time (get times 1)))))
 
-(defn timelength [timeStr]
-  (if (boolean timeStr)
-    (if (nil? (index-of timeStr "-"))
-      (int (* 60 (read-string (clojure.string/replace timeStr #"h" ""))))
-      (let [times (split timeStr #"-")]
-        (getMinutes times)))
+(defn timelength [time-str]
+  (if (boolean time-str)
+    (if (nil? (index-of time-str "-"))
+      (int (* 60 (read-string (clojure.string/replace time-str #"h" ""))))
+      (let [times (split time-str #"-")]
+        (get-minutes times)))
      0))
 
-(defn processLine [line]
-  (let [lineData (split line #"\s+")]
-    {:time (timelength (get lineData 2))
-     :date (get lineData 0)
-     :projectCode (get lineData 1)}))
+(defn process-line [line]
+  (let [line-date (split line #"\s+")]
+    {:time (timelength (get line-date 2))
+     :date (get line-date 0)
+     :project-code (get line-date 1)}))
 
-(defn filterEmptyRows [entries]
-  (filter #(false? (nil? (:projectCode %))) entries))
+(defn filter-empty-rows [entries]
+  (filter #(false? (nil? (:project-code %))) entries))
 
 (defn time-to-str [minutes]
   (str (quot minutes 60) " h " (mod minutes 60) " min"))
 
-(defn formatTime [totalMinutes]
-  (if (neg? totalMinutes)
-    (str "-" (time-to-str (Math/abs totalMinutes)))
-    (time-to-str totalMinutes)))
+(defn format-time [total-minutes]
+  (if (neg? total-minutes)
+    (str "-" (time-to-str (Math/abs total-minutes)))
+    (time-to-str total-minutes)))
 
-(defn calculateDiff [workdays totalMinutes]
-  (- totalMinutes (* workdays 450)))
+(defn calculate-diff [workdays total-minutes]
+  (- total-minutes (* workdays 450)))
 
-(defn getDayStats [date entries]
+(defn get-day-stats [date entries]
   (let [worktime (apply + (map :time (filter #(= date (:date %)) entries)))]
     (zipmap [:date :worktime :diff] [date worktime (- worktime 450)])))
 
-(defn getStatsForDays [dates entries]
-  (map #(getDayStats % entries) dates))
+(defn get-stats-for-day [dates entries]
+  (map #(get-day-stats % entries) dates))
 
 (defn print-day-stats [day-stats]
   (doseq [item day-stats]
     (println
-      (str (:date item) "\t" (formatTime (:worktime item)) "\t" (formatTime (:diff item))))))
+      (str (:date item) "\t" (format-time (:worktime item)) "\t" (format-time (:diff item))))))
 
-(defn readFile [filename initialDiff]
+(defn read-file [filename initial-diff]
   (with-open [rdr (reader filename)]
-    (let [entries (filterEmptyRows (map processLine (line-seq rdr)))
-          totalMinutes (apply + (map :time entries))
+    (let [entries (filter-empty-rows (map process-line (line-seq rdr)))
+          total-minutes (apply + (map :time entries))
           workdays (count (distinct (map :date entries)))
-          diff (+ initialDiff (calculateDiff workdays totalMinutes))]
-        (print-day-stats (getStatsForDays (distinct (map :date entries)) entries))
+          diff (+ initial-diff (calculate-diff workdays total-minutes))]
+        (print-day-stats (get-stats-for-day (distinct (map :date entries)) entries))
         (println)
-        (println (str "Total worktime: " (formatTime totalMinutes)))
-        (println (str "Difference: " (formatTime diff) " (" diff " min)")))))
+        (println (str "Total worktime: " (format-time total-minutes)))
+        (println (str "Difference: " (format-time diff) " (" diff " min)")))))
 
 (defn -main [& args]
   (let [file (:file (:options (parse-opts args cli-options)))
         diff (read-string (:diff (:options (parse-opts args cli-options))))]
-    (readFile file diff)))
+    (read-file file diff)))
--- test/tunnit/core_test.clj
+++ test/tunnit/core_test.clj
@@ -2,12 +2,12 @@
   (:require [midje.sweet :refer :all]
             [tunnit.core :refer :all]))
 
- (defn sampleData []
-   (list {:date "2018-02-01" :projectCode "p1" :time 135}
-         {:date "2018-02-01" :projectCode "p1" :time 315}
-         {:date "2018-02-02" :projectCode "p1" :time 100}
-         {:date "2018-02-03" :projectCode "p1" :time 450}
-         {:date "2018-02-03" :projectCode "p1" :time 10}))
+ (defn sample-data []
+   (list {:date "2018-02-01" :project-code "p1" :time 135}
+         {:date "2018-02-01" :project-code "p1" :time 315}
+         {:date "2018-02-02" :project-code "p1" :time 100}
+         {:date "2018-02-03" :project-code "p1" :time 450}
+         {:date "2018-02-03" :project-code "p1" :time 10}))
 
 (facts "about counting times"
   (fact "timelength returns correct minutes"
@@ -17,22 +17,22 @@
     (timelength "2h") => 120)
     (+ (timelength "09:20-11:00") (timelength "11:30-17:00")) => 430
   (fact "Format time is count correctly"
-    (formatTime 60) => "1 h 0 min"
-    (formatTime 65) => "1 h 5 min"
-    (formatTime 50) => "0 h 50 min"
-    (formatTime -20) => "-0 h 20 min")
+    (format-time 60) => "1 h 0 min"
+    (format-time 65) => "1 h 5 min"
+    (format-time 50) => "0 h 50 min"
+    (format-time -20) => "-0 h 20 min")
   (fact "Diff is calculated correctly"
-    (calculateDiff 2 900) => 0
-    (calculateDiff 2 870) => -30
-    (calculateDiff 2 920) => 20)
-  (fact "getDayStats correctly"
-    (getDayStats "2018-02-01" (sampleData)) => {:date "2018-02-01" :worktime 450 :diff 0}
-    (getDayStats "2018-02-02" (sampleData)) => {:date "2018-02-02" :worktime 100 :diff -350}
-    (getDayStats "2018-02-03" (sampleData)) => {:date "2018-02-03" :worktime 460 :diff 10})
+    (calculate-diff 2 900) => 0
+    (calculate-diff 2 870) => -30
+    (calculate-diff 2 920) => 20)
+  (fact "get-day-stats correctly"
+    (get-day-stats "2018-02-01" (sample-data)) => {:date "2018-02-01" :worktime 450 :diff 0}
+    (get-day-stats "2018-02-02" (sample-data)) => {:date "2018-02-02" :worktime 100 :diff -350}
+    (get-day-stats "2018-02-03" (sample-data)) => {:date "2018-02-03" :worktime 460 :diff 10})
   (fact "gets stats for given dates correctly"
-    (getStatsForDays
+    (get-stats-for-day
                       ["2018-02-01" "2018-02-02"]
-                      (sampleData)) => (list {
+                      (sample-data)) => (list {
                                                :date "2018-02-01"
                                                :worktime 450
                                                :diff 0}
@@ -41,6 +41,6 @@
                                                :worktime 100
                                                :diff -350}))
   (fact "diff renders correctly"
-    (formatTime
-      (calculateDiff
+    (format-time
+      (calculate-diff
         1 (+ (timelength "09:20-11:00") (timelength "11:30-17:00")))) => "-0 h 20 min"))
